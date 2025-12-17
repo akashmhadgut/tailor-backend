998c152bfb09bfd149a4e9cc0bce30fd977598b2
akashmhadgut <akashmhadgut280@gmail.com>
2025-12-17 14:26:42 +0530

    Initial backend upload

---
+++ src/routes/uploadRoutes.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename(req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    },
+});
+
+function checkFileType(file, cb) {
+    const filetypes = /jpg|jpeg|png|pdf|doc|docx/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        cb('Images and Documents only!');
+    }
+}
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 5000000 }, // 5MB
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    },
+});
+
+router.post('/', upload.array('files', 5), (req, res) => {
+    const filePaths = req.files.map(file => `/uploads/${file.filename}`);
+    res.send(filePaths);
+});
+
+export default router;
